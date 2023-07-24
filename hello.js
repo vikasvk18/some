0f0b7752baa6026c0b000daf81a75031ab58f615
vikasvk18 <abbidivikasreddy2000@gmail.com>
2023-07-24 23:23:47 +0530

    mbm

--- hello.js
+++ hello.js
@@ -2,4 +2,4 @@ console.log('hello')
 console.log(process.env.SOME_URL);
 console.log(process.env.MY_SECRET+'gkjgkj');
 let s=process.env.MY_SECRET;
-console.log(s.search('ttps://github.com/vikasvk18/some/settings/e'));
+console.log(s==='https://github.com/vikasvk18/some/settings/environments/931021921/edit');
