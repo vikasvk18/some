b6077a8fc2fffb73ff1ce0159afb774e0b5d4510
vikasvk18 <abbidivikasreddy2000@gmail.com>
2023-07-24 23:20:31 +0530

    khkj

--- hello.js
+++ hello.js
@@ -1,3 +1,5 @@
 console.log('hello')
 console.log(process.env.SOME_URL);
 console.log(process.env.MY_SECRET+'gkjgkj');
+let s=process.env.MY_SECRET;
+console.log(s.search('ttps://github.com/vikasvk18/some/settings/e'));
